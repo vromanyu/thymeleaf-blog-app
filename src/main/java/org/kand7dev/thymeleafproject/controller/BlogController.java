e8ed18cfbc68989dce111db5790d1635f38914c0
vromanyu <vromanyu@netcompany.com>
2025-04-24 10:26:45 +0300

    view post client side

--- src/main/java/org/kand7dev/thymeleafproject/controller/BlogController.java
+++ src/main/java/org/kand7dev/thymeleafproject/controller/BlogController.java
@@ -5,6 +5,7 @@ import org.kand7dev.thymeleafproject.service.PostService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @AllArgsConstructor
@@ -16,6 +17,12 @@ public class BlogController {
  public String viewBlogPosts(Model model){
   model.addAttribute("posts", postService.findAllPosts());
   return "blog/view_posts";
+ }
+
+ @GetMapping("/post/{postUrl}")
+ public String viewPost(@PathVariable String postUrl, Model model){
+  model.addAttribute("post", postService.findPostByUrl(postUrl));
+  return "blog/view_post";
  }
 
 }
