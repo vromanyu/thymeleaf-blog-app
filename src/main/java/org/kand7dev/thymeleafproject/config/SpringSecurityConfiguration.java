55c8af524822739c80a3e935caa133927030110e
vromanyu <vromanyu@outlook.com>
2025-04-25 09:27:59 +0300

    comment out CsrfTokenFilter
    it seems like Spring Security can eagerly fetch the CSRF token in the later versions. The application works properly without this filter.

--- src/main/java/org/kand7dev/thymeleafproject/config/SpringSecurityConfiguration.java
+++ src/main/java/org/kand7dev/thymeleafproject/config/SpringSecurityConfiguration.java
@@ -11,7 +11,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 
